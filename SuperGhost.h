3579ef40b56b09250a7bad87c4a781dd77f44949
edanglobus <edanglobus1@gmail.com>
2025-02-22 10:53:25 +0200

    Update SuperGhost.h

--- SuperGhost.h
+++ SuperGhost.h
@@ -10,9 +10,6 @@ private:
 public:
 	SuperGhost(char symbol, int x, int y) : Ghost(symbol, x, y) {};
 
-/*
-לחשוב אם לעשות לה שהיא לא יכולה לטפס יותר מגובה 0
-*/
 	virtual void move()override;
 	virtual bool check_probability_to_change_dir()const override{
 	return false;
